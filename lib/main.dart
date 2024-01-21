1bf0bcb144b8c7cc4ecff33d5992d2fed5b48504
abybrandon <abymazuya69@gmail.com>
2024-01-21 22:18:26 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:belajar_github/home.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: HomeView(),
+    );
+  }
+}
